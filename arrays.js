9f426e6b340a62632c50cb116cc24bbee84f8a72
Tanmay Anand <tanmayanand@gmail.com>
2020-06-20 16:52:47 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -35,3 +35,7 @@ function removeElementFromBeginningOfArray(array){
 function destructivelyRemoveElementFromEndOfArray(array){
     return array.pop()
 }
+
+function removeElementFromEndOfArray(array){
+    return array.slice(0,array.length-1);
+}
