ed67fa6af966c87dd03f1e35d94fb616863362ff
Tanmay Anand <tanmayanand@gmail.com>
2020-06-20 16:48:11 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,33 @@
+var chocolateBars=["snickers","hundred grand","kitkat","skittles"]
+
+function destructivelyAddElementToBeginningOfArray(array1,element1){
+    array1.unshift(element1)
+return array1
+}
+
+function addElementToBeginningOfArray(array2,element2){
+    array2 = [element2,...array2]
+    return array2
+}
+
+function destructivelyAddElementToEndOfArray(array1,element1){
+    array1.push(element1)
+return array1
+}
+
+function addElementToEndOfArray(array2,element2){
+    array2 = [...array2,element2]
+    return array2
+}
+function accessElementInArray(array,index){
+    return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(Array){
+    Array.shift();
+    return Array;
+}
+
+function removeElementFromBeginningOfArray(array){
+    return array.slice(1)
+}
